1e29007e59ad2c69ffdae5cdda07b774c3156b18
Zohair Kaleem <zohairkaleem96@gmail.com>
2025-05-09 18:45:53 +0500

    UI changes still needs some work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,28 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(246)))), ((int)(((byte)(233)))));
             this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
             this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
             this.flowLayoutPanel1.Controls.Add(this.label1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(421, 640);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(417, 640);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(246)))), ((int)(((byte)(233)))));
+            this.flowLayoutPanel3.Controls.Add(this.label2);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(418, 100);
+            this.flowLayoutPanel3.TabIndex = 2;
+            this.flowLayoutPanel3.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel3_Paint_1);
+            this.flowLayoutPanel3.Resize += new System.EventHandler(this.flowLayoutPanel3_Resize_1);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(50, 0, 30, 0);
+            this.label2.Size = new System.Drawing.Size(418, 100);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chat Ned";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(246)))), ((int)(((byte)(233)))));
+            this.flowLayoutPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.flowLayoutPanel2.Controls.Add(this.textBox2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 109);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(412, 501);
+            this.flowLayoutPanel2.TabIndex = 1;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 36);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Search or start a new chat";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            this.textBox2.MouseEnter += new System.EventHandler(this.textBox2_MouseEnter);
+            this.textBox2.MouseLeave += new System.EventHandler(this.textBox2_MouseLeave);
             // 
             // label1
             // 
@@ -62,51 +132,186 @@
             this.label1.TabIndex = 0;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // flowLayoutPanel2
+            // panel1
             // 
-            this.flowLayoutPanel2.AutoScroll = true;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 109);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(412, 501);
-            this.flowLayoutPanel2.TabIndex = 1;
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(417, 540);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(851, 100);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // flowLayoutPanel3
+            // button1
             // 
-            this.flowLayoutPanel3.Controls.Add(this.label2);
-            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
-            this.flowLayoutPanel3.Size = new System.Drawing.Size(418, 100);
-            this.flowLayoutPanel3.TabIndex = 2;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(688, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label2
+            // textBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Script", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(3, 0);
-            this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(50, 0, 30, 0);
-            this.label2.Size = new System.Drawing.Size(280, 53);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Chat NED";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox1.Location = new System.Drawing.Point(16, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(666, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter Message";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter_1);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(417, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(851, 100);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(153, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 29);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "User";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(4, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(89, 90);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(153, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 28);
+            this.label3.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menuStrip1.Size = new System.Drawing.Size(92, 19);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.TabStop = true;
+            this.menuStrip1.Text = "miniToolStrip";
+            this.menuStrip1.Visible = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(421, 106);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(847, 428);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(60, 243);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(181, 62);
+            this.panel6.TabIndex = 1;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(640, 287);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(181, 62);
+            this.panel5.TabIndex = 0;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1268, 640);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
             this.flowLayoutPanel3.ResumeLayout(false);
-            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
+
+        private System.Windows.Forms.Panel panel6;
+
+        private System.Windows.Forms.Panel panel5;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.Panel panel4;
+
+        private System.Windows.Forms.Panel panel3;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Panel panel1;
 
         #endregion
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
-
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace WindowsFormsApp1
 {
@@ -22,6 +23,20 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //resizing logic
+            panel2.Dock = DockStyle.Top;  // Top panel
+            panel1.Dock = DockStyle.Bottom;
+            panel4.Dock = DockStyle.Fill;
+            // Place both controls inside panel1 manually or via Designer
+            textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+            button1.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+            
+            
+            ApplyRoundedCorners(panel5,20);
+            ApplyRoundedCorners(panel6,20);
+            
+
+            //connection logic
             OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.Oledb.12.0;Data Source=C:\Users\sukku\OneDrive\Documents\ChatApp.accdb;");
             OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT Username from Users", con);
             con.Open();
@@ -52,6 +67,7 @@ namespace WindowsFormsApp1
 
         private void label1_Click(object sender, EventArgs e)
         {
+            
 
         }
 
@@ -62,7 +78,229 @@ namespace WindowsFormsApp1
 
         private void label2_Click(object sender, EventArgs e)
         {
+            // label2.TextAlign = ContentAlignment.MiddleCenter;
+            
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+            
+            
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            // textBox1.TextAlign=ContentAlignment.MiddleLeft;
+            
+        }
+        
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            //placeholder text logic p1
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Enter Message";
+                textBox1.ForeColor= Color.Gray;
+            }
+        }
+
+        private void textBox1_Enter_1(object sender, EventArgs e)
+        {
+            //placeholder text logic p2
+            if (textBox1.Text == "Enter Message")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor= Color.Black;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Color borderColor = Color.Gray;
+            int borderWidth = 1;
+            ControlPaint.DrawBorder(e.Graphics, panel2.ClientRectangle,
+                borderColor, borderWidth, ButtonBorderStyle.Solid,
+                borderColor, borderWidth, ButtonBorderStyle.Solid,
+                borderColor, borderWidth, ButtonBorderStyle.Solid,
+                borderColor, borderWidth, ButtonBorderStyle.Solid);
+        
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            // panel3.BackgroundImage=Image.FromFile();
+            
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void flowLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void flowLayoutPanel3_Resize(object sender, EventArgs e)
+        {
+            // Check if label2 exists in flowLayoutPanel3
+            if (flowLayoutPanel3.Controls.Contains(label2))
+            {
+                // Update label2 width if AutoSize is enabled
+                label2.AutoSize = true;
+
+                // Center label2 horizontally
+                int centerX = (flowLayoutPanel3.Width - label2.Width) / 2;
+                label2.Margin = new Padding(centerX, label2.Margin.Top, 0, 0);
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void flowLayoutPanel3_Paint_1(object sender, PaintEventArgs e)
+        {
+           
+            
+        }
+
+        private void flowLayoutPanel3_Resize_1(object sender, EventArgs e)
+        {
+            if (flowLayoutPanel3.Controls.Contains(label2))
+            {
+                // Make sure AutoSize is set to false for label2
+                label2.AutoSize = false;
+
+                // Set the label size to its current width and height (or define it)
+                label2.Width = flowLayoutPanel3.Width;
+
+                // Center label2 horizontally inside flowLayoutPanel3
+                label2.Location = new Point((flowLayoutPanel3.Width - label2.Width) / 2, label2.Location.Y);
+            }
+        }
+        
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            //placeholder text logic p2
+            if (textBox2.Text == "Search or start a new chat")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor= Color.Black;
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Search or start a new chat";
+                textBox2.ForeColor= Color.Gray;
+            }
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            //this is for the name of the person we talkin to ,its size would 
+            //automatically adapt to the width of name
+            
+        }
+
+
+        private void textBox2_MouseEnter(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.LightGray;
+        }
+
+        private void textBox2_MouseLeave(object sender, EventArgs e)
+        {
+            textBox2.BackColor= Color.White;
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.LightGray;
+            
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+        }
+        private void ApplyRoundedCorners(Panel panel, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            Rectangle rect = new Rectangle(0, 0, panel.Width, panel.Height);
+            int diameter = radius * 2;
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+            panel.Region = new Region(path);
+        }
+
+        private void addbordercolor(Panel panel,PaintEventArgs e)
+        {
+            int radius = 20;
+            Rectangle rect = new Rectangle(1, 1, panel.Width - 2, panel.Height - 2);
+            int diameter = radius * 2;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+                path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+                path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+                path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+                path.CloseFigure();
+
+                using (Pen borderPen = new Pen(Color.DarkBlue, 2))  // Change color/thickness here
+                {
+                    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    e.Graphics.DrawPath(borderPen, path);
+                }
+            }
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            addbordercolor(panel5,e);
+           
+            
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+            addbordercolor(panel6,e);
+            
         }
     }
 }
